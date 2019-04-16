3a670950c96092a6674dc7d9bc72b0b72c75780e
milozzz <1750886@qq.com>
2019-04-16 22:21:12 +1000

    comment

---
+++ nuxt-demo/pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-xs-center">
+        <h1>firstName</h1>
+        <h1>lastName</h1>
+        <h1>number</h1>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  },
+  mounted() {
+    console.log(this.$route)
+  },
+}
+</script>
+<style>
+  .text-xs-center{
+    margin-top:200px;
+  }
+</style>
---
+++ nuxt-demo/pages/user/_desc.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-xs-center">
+        <h1 v-if='desc[0]'>firstName: {{desc[0].substring(0,1).toUpperCase()+desc[0].substring(1)}}</h1>
+        <h1 v-if='desc[0]'>lastName: {{desc[1].substring(0,1).toUpperCase()+desc[1].substring(1)}}</h1>
+        <h1>user id: {{desc[2]}}</h1>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+
+// get the router information
+
+export default {
+  data(){
+    return{
+      desc:[]
+    }
+  },
+  components: {
+  },
+  mounted() {
+    this.desc=this.$route.params.desc.split("-") // split with "-" and get name and id
+  },
+}
+</script>
+<style>
+.text-xs-center{
+  margin-top:200px;
+}
+</style>
